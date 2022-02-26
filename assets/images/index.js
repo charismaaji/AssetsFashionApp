a8d80a294085c2cc59281f070f8bbece0a07ba1f
Charisma Kurniawan Aji <charismakurniawan@gmail.com>
2022-02-27 01:39:59 +0700

    First commit

---
+++ assets/images/index.js
@@ -0,0 +1,29 @@
+import slider1 from './slider1.jpeg';
+import slider2 from './slider2.jpeg';
+import slider3 from './slider3.jpeg';
+import gift from './gift.png';
+import influencer from './influencer.png';
+import categoriesDesigner from './categoriesDesigner.png';
+import categoriesWomenSuite from './categoriesWomenSuite.png';
+import categoriesShoes from './categoriesShoes.png';
+import categoriesHijab from './categoriesHijab.png';
+import categoriesSport from './categoriesSport.png';
+import BCACard from './BCACard.jpg';
+import DBSCard from './VisaCard.jpg';
+import AvatarImage from './profileBackground.png';
+
+export {
+  slider1,
+  slider2,
+  slider3,
+  gift,
+  influencer,
+  categoriesDesigner,
+  categoriesWomenSuite,
+  categoriesShoes,
+  categoriesHijab,
+  categoriesSport,
+  BCACard,
+  DBSCard,
+  AvatarImage,
+};
---
+++ assets/productImage/index.js
@@ -0,0 +1,35 @@
+import ProductA1 from './ProductA1.png';
+import ProductA2 from './ProductA2.png';
+import ProductA3 from './ProductA3.png';
+import ProductA4 from './ProductA4.png';
+import ProductB1 from './ProductB1.png';
+import ProductB2 from './ProductB2.png';
+import ProductB3 from './ProductB3.png';
+import ProductB4 from './ProductB4.png';
+import ProductC1 from './ProductC1.png';
+import ProductC2 from './ProductC2.png';
+import ProductC3 from './ProductC3.png';
+import ProductC4 from './ProductC4.png';
+import ProductD1 from './ProductD1.png';
+import ProductD2 from './ProductD2.png';
+import ProductD3 from './ProductD3.png';
+import ProductD4 from './ProductD4.png';
+
+export {
+  ProductA1,
+  ProductA2,
+  ProductA3,
+  ProductA4,
+  ProductB1,
+  ProductB2,
+  ProductB3,
+  ProductB4,
+  ProductC1,
+  ProductC2,
+  ProductC3,
+  ProductC4,
+  ProductD1,
+  ProductD2,
+  ProductD3,
+  ProductD4,
+};
